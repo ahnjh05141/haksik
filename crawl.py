67282662d1d2059efa3c913b1c309d98b1c9a4c5
ahnjh05141 <jaehyuns05141@gmail.com>
2025-04-07 22:19:56 +0900

    feat: make it lighter

--- crawl.py
+++ crawl.py
@@ -5,61 +5,50 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 import time
 
-# CHROME_PATH = "/nix/store/zi4f80l169xlmivz8vja8wlphq74qqk0-chromium-125.0.6422.141/bin/chromium"
-# CHROMEDRIVER_PATH = "/nix/store/3qnxr5x6gw3k9a9i7d0akz0m6bksbwff-chromedriver-125.0.6422.141/bin/chromedriver"
-
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-
-# options.binary_location = CHROME_PATH
-# service = Service(executable_path=CHROMEDRIVER_PATH)
-
-driver = webdriver.Chrome(options=options)
-
 
 def get_menu_310():
-    menus = []
+    options = Options()
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless=new") 
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-extensions")
 
-    driver.get("https://mportal.cau.ac.kr/main.do")
-    time.sleep(2)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver = webdriver.Chrome(options=options)
 
-    # lunch_element = driver.find_element(By.XPATH, "//*[contains(text(), '조식')]")
-    # location = lunch_element.location
-    # size = lunch_element.size
+    try:
+        menus = []
 
-    # ActionChains(driver).move_to_element_with_offset(lunch_element, 50, 0).click().perform()
-    # time.sleep(0.1)
+        driver.get("https://mportal.cau.ac.kr/main.do")
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    html_text = driver.page_source
+        html_text = driver.page_source
 
-    # with open("result.txt", "w", encoding="utf-8") as f:
-    #     f.write(html_text)
+        menu1 = html_text.split("참슬기식당(310관 B4층)")[2]
+        menu1 = menu1.split("\n")[7]
+        menu1 = menu1.split("<p>")[1:]
 
-    menu1 = html_text.split("참슬기식당(310관 B4층)")[2]
-    menu1 = menu1.split("\n")[7]
-    menu1 = menu1.split("<p>")[1:]
+        menu2 = html_text.split("참슬기식당(310관 B4층)")[3][:1000]
+        menu2 = menu2.split("\n")[7]
+        menu2 = menu2.split("<p>")[1:]
 
-    menu2 = html_text.split("참슬기식당(310관 B4층)")[3][:1000]
-    menu2 = menu2.split("\n")[7]
-    menu2 = menu2.split("<p>")[1:]
+        temp = []
+        for l in menu1:
+            l = l.split("</p>")[0]
+            temp.append(l)
+        menus.append(temp)
 
-    temp = []
-    for l in menu1:
-        l = l.split("</p>")[0]
-        temp.append(l)
-    menus.append(temp)
+        temp = []
+        for l in menu2:
+            l = l.split("</p>")[0]
+            temp.append(l)
+        menus.append(temp)
+        return menus
 
-    temp = []
-    for l in menu2:
-        l = l.split("</p>")[0]
-        temp.append(l)
-    menus.append(temp)
+    finally:
+        driver.quit()
 
-    time.sleep(3.0)
-    driver.quit()
-
-    print(menus)
-    return menus
+get_menu_310()
